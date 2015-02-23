8875773c043f4121fe9bf43b1812505cd2e625cd
Andy <Andy@mail.com>
2015-02-23 08:22:00 -0500

    return null; // obj_type exist, exit the function, crud.js at page 295

--- webapp/chapter8/lib/crud.js
+++ webapp/chapter8/lib/crud.js
@@ -2,7 +2,7 @@
 'use strict';
 var checkType, constructObj, readObj, updateObj,
     destroyObj;
-var loadSchema, checkShema, clearIsOnline;
+var loadSchema, checkSchema, clearIsOnline;
 var mongodb = require('mongodb');
 var fsHandle = require('fs');
 var JSV = require('JSV').JSV;
@@ -21,11 +21,29 @@ loadSchema = function( schema_name, schema_path ) {
         // convert JSON into javascript object
         objTypeMap[schema_name] = JSON.parse( data );
     } );
-}
+};
+
+checkSchema = function (obj_type, obj_map, callback) {
+    var schema_map = objTypeMap[ obj_type ];
+    var report_map = validator.validate(obj_map, schema_map);
+    callback(report_map.errors);
+};
+
+// make sure all user is mark as offline when Mongo server starts
+clearIsOnline = function() {
+    updateObj( 'user', {is_online: true}, {is_online: false},
+        function( response_map ) {
+            console.log('All users are offline', response_map);
+        });
+};
 // ---------------- END UTILITY METHODS -----------------
 
 // ---------------- BEGIN PUBLIC METHODS ------------------
-checkType = function () {
+checkType = function ( obj_type ) {
+    if( !objTypeMap[obj_type] ) {
+        return ({error_msg: 'Object type ' + obj_type + ' is not supported'});
+    }
+    return null; // obj_type exist, exit the function
 };
 
 // use CONSTRUCT as our method name, CREATE is a Javascript reserve word
