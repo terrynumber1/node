2af853b58c5dd57e1b0d2c66ec1f61f4765fbd5d
VM3 Name <opsvm3user@opsvm3.turner.com>
2015-04-07 06:24:23 -0400

    Added todo.jade

--- a/express-styl/app.js
+++ a/express-styl/app.js
@@ -11,12 +11,12 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');   // setup jade folder
+  app.set('view engine', 'jade');           // set view engine to jade
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));   // static file folder
 });
 
 app.configure('development', function(){
@@ -32,6 +32,7 @@ app.configure('production', function(){
 app.get('/', routes.index);         // folder /routes/routes.js exported index function
 app.get('/about', routes.about);    // folder /routes/routes.js about exported function
 app.get('/todo', routes.todo);
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
