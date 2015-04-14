a468b197fb9fed907984df661c67aa3d39afcc9d
VM3 Name <opsvm3user@opsvm3.turner.com>
2015-04-14 07:27:11 -0400

    nodetest2 working, but no update

--- a/nodetest2/routes/users.js
+++ a/nodetest2/routes/users.js
@@ -10,4 +10,33 @@ router.get('/userlist', function (req, res) {
     });
 });
 
+/* POST to adduser */
+router.post('/adduser', function (req, res) {
+    var db = req.db;
+
+    // userlist collection is the name in MongoDB
+    db.collection('userlist').insert(req.body, function (err, result) {
+        res.send(
+            (err === null ? {msg: ''} : {msg: err} )
+        );
+    });
+});
+
+/* DELETE to deleteuser */
+router.delete('/deleteuser/:id', function (req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    // DEBUG
+    console.log(userToDelete);
+
+    db.collection('userlist').removeById(userToDelete, function (err, result) {
+        res.send( (result === 1) ? {msg: ''} : {msg: 'error: ' + err} );
+    });
+});
+
+
 module.exports = router;
+
+HERE
+http://scottksmith.com/blog/2014/05/05/beer-locker-building-a-restful-api-with-node-crud/
+make a new project with mongoose
